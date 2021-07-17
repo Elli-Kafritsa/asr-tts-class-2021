54c55616c35f040a24c6d631a210e6b6b2568db0
Elli Kafritsa <73034128+Elli-Kafritsa@users.noreply.github.com>
2021-07-17 22:33:42 +0300

    Update spanish_data_prep.sh

--- asr-recipes/elvina_recipe/s5/local/spanish_data_prep.sh
+++ asr-recipes/elvina_recipe/s5/local/spanish_data_prep.sh
@@ -1,50 +1,57 @@
 #!/usr/bin/env bash
 
-# Copyright 2021 NLP chicas
+# Copyright 2021 NLP chicas 
 # Apache 2.0
+# For the train/development data
 
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <src-dir> <part-info> <dst-dir>"
-  echo "e.g.: $0 other/data/voxforge_spanish elvina_voxforge_train.tsv data/train"
-  exit 1
-fi
 
-#arguments:
-src=$1
-part=$2
-dst=$3
+PYTHONIOENCODING=UTF-8 python3 process1.py
+PYTHONIOENCODING=UTF-8 python3 dev_train_split.py 
+
+src_data=../utils
+dev_dir=data/dev
+train_dir=data/train
 
 # create destination directory, else exit
-mkdir -p $dst || exit 1;
+mkdir -p $dev_dir || exit 1;
+mkdir -p $train_dir || exit 1;
 
 # if source directory doesn't exist pop error
-[ ! -d $src ] && echo "$0: no such directory $src" && exit 1;
-
-# if these files exist, delete them and rewrite them
-wav_scp=$dst/wav.scp; [[ -f "$wav_scp" ]] && rm $wav_scp
-trans=$dst/text; [[ -f "$trans" ]] && rm $trans
-utt2spk=$dst/utt2spk; [[ -f "$utt2spk" ]] && rm $utt2spk
-
-# Process tsv files to extract all required information
-# create a python file that processes tsv files that
-# takes as input the part, the wav_scp (where it is going to store the transcriptions)
-# and the utt2spk (where it is going to store the speaker's utterance)
-PYTHONIOENCODING=UTF-8 python3 local/process1.py  $part $src/clips $wav_scp $trans $utt2spk
-python3 local/dev_train_split.py 
-
-spk2utt=$dst/spk2utt
-utils/utt2spk_to_spk2utt.pl <$utt2spk >$spk2utt || exit 1
-
+[ ! -d $dev_dir ] && echo "$0: no such directory $dev_dir" && exit 1;
+[ ! -d $train_dir ] && echo "$0: no such directory $train_dir" && exit 1;
 
 # Data validation
-ntrans=$(wc -l <$trans)
-nutt2spk=$(wc -l <$utt2spk)
+trans_dev=$dev_dir/text
+trans_train=$train_dir/text
+spk2utt_dev=$dev_dir/spk2utt
+utt2spk_dev=$dev_dir/utt2spk
+spk2utt_train=$train_dir/spk2utt
+utt2spk_train=$train_dir/utt2spk
 
-! [ "$ntrans" -eq "$nutt2spk" ] && \
-echo "Inconsistent #transcripts($ntrans) and #utt2spk($nutt2spk)" && exit 1;
+$src_data/utt2spk_to_spk2utt.pl <$utt2spk_dev >$spk2utt_dev || exit 1
+$src_data/utt2spk_to_spk2utt.pl <$utt2spk_train >$spk2utt_train || exit 1
 
-utils/validate_data_dir.sh --no-feats $dst || exit 1;
 
-echo "$0: successfully prepared data in $dst"
+# For the dev set
+ntrans_dev=$(wc -l <$trans_dev)
+nutt2spk_dev=$(wc -l <$utt2spk_dev)
+! [ "$ntrans_dev" -eq "$nutt2spk_dev" ] && \
+  echo ["Inconsistent #transcripts($ntrans_dev) and #utt2spk($nutt2spk_dev)"] && exit 1;
+
+$src_data/validate_data_dir.sh --no-feats $dev_dir || exit 1;
+
+echo "$0: successfully prepared data in $dev_dir"
+
+
+
+# For the train set
+ntrans_train=$(wc -l <$trans_train)
+nutt2spk_train=$(wc -l <$utt2spk_train)
+! [ "$ntrans_train" -eq "$nutt2spk_train" ] && \
+  echo "Inconsistent #transcripts($ntrans_train) and #utt2spk($nutt2spk_train)" && exit 1;
+
+$src_data/validate_data_dir.sh --no-feats $train_dir || exit 1;
+
+echo "$0: successfully prepared data in $train_dir"
 
 exit 0
